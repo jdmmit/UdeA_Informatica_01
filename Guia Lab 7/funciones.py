209052dacfb476bd425cb6654c3aa1e4250442ce
jdmmit <juandavidmurillomejia0718@outlook.es>
2025-06-22 16:44:54 -0500

    22/06/2025 01

---
+++ Guia Lab 7/funciones.py
@@ -0,0 +1,9 @@
+"""
+
+FUNCIONES.PY - Lab 7 Estadísticas Estudiantes 
+Contiene todas las funciones para el procesamiento de datos
+Autor: Juan David Murillo & Juan Camilo Medina
+
+"""
+
+
---
+++ Guia Lab 7/main.py
@@ -0,0 +1,22 @@
+"""
+MAIN.PY - Lab 7 Estadísticas Estudiantes 
+Contiene todas las funciones para el procesamiento de datos
+Autor: Juan David Murillo & Juan Camilo Medina
+Curso: Informática I - Universidad de Antioquia
+
+INSTRUCCIONES PARA EJECUTAR:
+1. Asegúrate de tener los archivos: notas_estudiantes.csv y hist_matriculados.csv
+2. Asegúrate de tener el archivo: funciones.py en la misma carpeta
+3. Instala las librerías necesarias: pip install numpy matplotlib
+4. Ejecuta: python main.py
+
+NUEVAS CARACTERÍSTICAS:
+- Gráficos automáticos en opciones 4, 5 y 6
+- Visualización profesional de datos
+- Interfaz mejorada con indicadores visuales
+"""
+
+# Importar todas las funciones del módulo funciones
+from funciones import *
+
+
